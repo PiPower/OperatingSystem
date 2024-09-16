78357da4fc2f72c28627e323d992dd27d56de6df
PiPower <darthlord.pl@gmail.com>
2024-09-16 21:10:02 +0200

    transition to pm working fine !!!!

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include "memory_mappings.h"
-
+#include "memory_layout.h"
 # define VIDEO_ADDRESS 0xB8000
+
 
 void main()
 {
-    char* video_mem = (char*) VIDEO_ADDRESS;
-    *video_mem = 'd';
+    char entry_msg[] = { 'h', 'e', 'l', 'l', 'o',' ', 'f', 'r', 'o', 'm', ' ', 'k', 'e', 'r', 'n',  'e', 'l', '\0'};
+    char* video_mem = (char*)VIDEO_ADDRESS;
+
+    int i =0;
+    while (entry_msg[i] != '\0')
+    {
+        video_mem[2 * i] = entry_msg[i];
+        i++;
+    }
+    
     while (1)
     {
       
