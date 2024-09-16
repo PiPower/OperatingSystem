98bd5d65def463d25a02eaf6301db66e04836a74
PiPower <darthlord.pl@gmail.com>
2024-09-16 19:58:24 +0200

    non working base transition to pm

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "memory_mappings.h"
 
+# define VIDEO_ADDRESS 0xB8000
+
 void main()
 {
+    char* video_mem = (char*) VIDEO_ADDRESS;
+    *video_mem = 'd';
+    while (1)
+    {
+      
+    }
+    
     return;
 }
